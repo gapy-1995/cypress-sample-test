591f2933cd13fa322f958b192bbf2b9ca929e227
locnt <locnguyent3108@gmail.com>
2024-03-19 09:38:06 +0700

    add more test case

---
+++ cypress/e2e/automationExercise/AP_LoginWithInvalidUser.cy.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+describe('As an fragment user, i can not login to automation exercise website', ()=>{
+    it('AP test case, implement later', () => {
+        expect(true).eql(false)
+        console.log('This is a failed test');
+    })
+
+})
---
+++ cypress/e2e/saucedemo/TC2_FailedTestCase.cy.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+describe('As an fragment user, i can not login to automation exercise website', ()=>{
+    it('Failed test case, Need fix soon ', () => {
+        expect(true).eql(false)
+        console.log('This is a failed test');
+    })
+
+})
